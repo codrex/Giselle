8d12589383de00d4e6f24cb18c08ec464082437d
Rex Ogbemudia <rex.ogbemudia@andela.com>
2019-01-30 05:03:09 +0100

    Updates

---
+++ precache-manifest.e8850f51604b63dd910c07da331b94f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab48928f89c2ecba2cb12d66ffc8110e",
+    "url": "/giselle/static/media/hinted-GorditaRegular.ab48928f.ttf"
+  },
+  {
+    "revision": "8e20a94bec6ecaba9492b6ebc3cd7a58",
+    "url": "/giselle/static/media/hinted-GorditaMedium.8e20a94b.ttf"
+  },
+  {
+    "revision": "34d96ae252ecc4bda88eaafda241fc1b",
+    "url": "/giselle/static/media/hinted-GorditaBold.34d96ae2.ttf"
+  },
+  {
+    "revision": "b19845472990d6a9f6d4e02202b5de3d",
+    "url": "/giselle/static/media/fakt.b1984547.ttf"
+  },
+  {
+    "revision": "d12342ed7b1a831ff4a064e6a4fd1b8c",
+    "url": "/giselle/static/media/downArrow.d12342ed.svg"
+  },
+  {
+    "revision": "01f399489f856d43b7fe",
+    "url": "/giselle/static/js/runtime~main.01f39948.js"
+  },
+  {
+    "revision": "c388880bd5b09083e537",
+    "url": "/giselle/static/js/main.c388880b.chunk.js"
+  },
+  {
+    "revision": "0000add5fb7c923c8d7e",
+    "url": "/giselle/static/js/1.0000add5.chunk.js"
+  },
+  {
+    "revision": "c388880bd5b09083e537",
+    "url": "/giselle/static/css/main.3805974a.chunk.css"
+  },
+  {
+    "revision": "e1a68e86674ba71ed1416c53069f5b23",
+    "url": "/giselle/index.html"
+  }
+];
